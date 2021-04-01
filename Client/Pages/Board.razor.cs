8f8ccc23e92ec297634738886d7d3010fb319f00
Mark Williams <amwilliams137@gmail.com>
2021-04-02 00:10:16 +0100

    Rubbish hexagonal spots

---
+++ Client/Pages/Board.razor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Pages
+{
+    public partial class Board
+    {
+        private ICollection<Vertex> Vertices;
+
+        protected override void OnInitialized()
+        {
+            Vertices = GenerateVertices();
+        }
+
+        private static ICollection<Vertex> GenerateVertices()
+        {
+            return new List<Vertex>
+            {
+                new Vertex { X = 0, Y = 0 },
+                new Vertex { X = 8, Y = 0 },
+                new Vertex { X = -8, Y = 0 },
+                new Vertex { X = 4, Y = -4 * Math.Sqrt(3) },
+                new Vertex { X = -4, Y = -4 * Math.Sqrt(3) },
+                new Vertex { X = 4, Y = 4 * Math.Sqrt(3) },
+                new Vertex { X = -4, Y = 4 * Math.Sqrt(3) },
+            };
+        }
+
+        private class Vertex
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+        }
+    }
+}
