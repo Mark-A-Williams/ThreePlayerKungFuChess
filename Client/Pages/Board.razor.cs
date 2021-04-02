a3fd589501200df9b1d678e83956578204115853
Mark Williams <amwilliams137@gmail.com>
2021-04-02 12:49:12 +0100

    More maths

--- Client/Pages/Board.razor.cs
+++ Client/Pages/Board.razor.cs
@@ -18,8 +18,8 @@ namespace Client.Pages
         {
             var vertices = GenerateBottomThirdOfVertices();
 
-            vertices.AddRange(GenerateBottomThirdOfVertices().Select(o => o.RotateAroundOrigin(120)));
-            vertices.AddRange(GenerateBottomThirdOfVertices().Select(o => o.RotateAroundOrigin(-120)));
+            //vertices.AddRange(GenerateBottomThirdOfVertices().Select(o => o.RotateAroundOrigin(120)));
+            //vertices.AddRange(GenerateBottomThirdOfVertices().Select(o => o.RotateAroundOrigin(-120)));
 
             return vertices.Distinct().ToArray();
         }
@@ -28,14 +28,43 @@ namespace Client.Pages
         {
             var vertices = new List<Vertex>();
 
-            for (int i = -4; i <= 4; i++)
-            {
-                vertices.Add(new Vertex { X = i, Y = -4 * Math.Sqrt(3) });
-            }
+            vertices.AddRange(InterpolateRangeOfVertices(
+                new Vertex { X = 0, Y = 0 },
+                new Vertex { X = 0, Y = -4 * Math.Sqrt(3) },
+                4));
 
-            vertices.Add(new Vertex { X = 0, Y = 0 });
+            vertices.AddRange(InterpolateRangeOfVertices(
+                new Vertex { X = -6, Y = -2 * Math.Sqrt(3) },
+                new Vertex { X = -4, Y = -4 * Math.Sqrt(3) },
+                4));
+
+            vertices.AddRange(InterpolateRangeOfVertices(
+                new Vertex { X = 6, Y = -2 * Math.Sqrt(3) },
+                new Vertex { X = 4, Y = -4 * Math.Sqrt(3) },
+                4));
 
             return vertices;
+        }
+
+        private static ICollection<Vertex> InterpolateRangeOfVertices(Vertex end1, Vertex end2, int number)
+        {
+            // number = 4 results in 5 points - divides the range into 4 parts incl. 2 ends
+
+            var result = new List<Vertex> { end1 };
+
+            for (int i = 1; i < number; i++)
+            {
+                var coeff = (double) i / number;
+                var newVertex = new Vertex 
+                { 
+                    X = end1.X + (end2.X - end1.X) * coeff, 
+                    Y = end1.Y + (end2.Y - end1.Y) * coeff 
+                };
+                result.Add(newVertex);
+            }
+
+            result.Add(end2);
+            return result;
         }
 
         private class Vertex
