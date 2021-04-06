e62d6e2c9af1c66a5bce5110e589cae70806dc83
Mark Williams <amwilliams137@gmail.com>
2021-04-06 17:25:16 +0100

    stuff

--- Client/Pages/Board.razor.cs
+++ Client/Pages/Board.razor.cs
@@ -14,39 +14,48 @@ namespace Client.Pages
             Vertices = GenerateVertices();
         }
 
+        // what am I doing
+        // Just have the board as a static image
+        // Have something shared which maps a tile e.g. H9 to a coord
+
         private static ICollection<Vertex> GenerateVertices()
         {
-            var vertices = GenerateBottomThirdOfVertices();
+            // Honestly not sure if this is even useful
 
-            //vertices.AddRange(GenerateBottomThirdOfVertices().Select(o => o.RotateAroundOrigin(120)));
-            //vertices.AddRange(GenerateBottomThirdOfVertices().Select(o => o.RotateAroundOrigin(-120)));
+            var vertices = GenerateBottomLeftVertices();
+
+            for (int i = 60; i <= 300; i += 60)
+            {
+                vertices.AddRange(GenerateBottomLeftVertices().Select(o => o.RotateAroundOrigin(i)));
+            }
 
             return vertices.Distinct().ToArray();
         }
 
-        private static List<Vertex> GenerateBottomThirdOfVertices()
+        private static List<Vertex> GenerateBottomLeftVertices()
         {
-            var vertices = new List<Vertex>();
-
-            vertices.AddRange(InterpolateRangeOfVertices(
+            var centreVertices = InterpolateRangeOfVertices(
                 new Vertex { X = 0, Y = 0 },
                 new Vertex { X = 0, Y = -4 * Math.Sqrt(3) },
-                4));
+                4);
 
-            vertices.AddRange(InterpolateRangeOfVertices(
+            var leftSideVertices = InterpolateRangeOfVertices(
                 new Vertex { X = -6, Y = -2 * Math.Sqrt(3) },
                 new Vertex { X = -4, Y = -4 * Math.Sqrt(3) },
-                4));
+                4);
 
-            vertices.AddRange(InterpolateRangeOfVertices(
-                new Vertex { X = 6, Y = -2 * Math.Sqrt(3) },
-                new Vertex { X = 4, Y = -4 * Math.Sqrt(3) },
-                4));
+            var vertices = new List<Vertex>();
+
+            for (int i = 0; i <= 4; i++)
+            {
+                vertices.AddRange(InterpolateRangeOfVertices(
+                    leftSideVertices[i], centreVertices[i], 4));
+            }
 
             return vertices;
         }
 
-        private static ICollection<Vertex> InterpolateRangeOfVertices(Vertex end1, Vertex end2, int number)
+        private static IList<Vertex> InterpolateRangeOfVertices(Vertex end1, Vertex end2, int number)
         {
             // number = 4 results in 5 points - divides the range into 4 parts incl. 2 ends
 
@@ -90,7 +99,12 @@ namespace Client.Pages
                     return false;
                 }
 
-                return (X == vertex.X) && (Y == vertex.Y);
+                return X == vertex.X && Y == vertex.Y;
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(X, Y);
             }
 
             public Vertex RotateAroundOrigin(double anticlockwiseAngleDegrees)
